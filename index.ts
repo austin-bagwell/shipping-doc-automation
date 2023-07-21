63d7920f76ca40b7ad9a439a9769cbc261e7c9c0
austin-bagwell <austinbagwell.dev@gmail.com>
2023-07-20 21:26:24 -0600

    add silly node input script. useful later to make a cli tool?

--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
 import dotenv from "dotenv";
+import * as readline from "readline";
 
 import { makeBolRequest } from "./src/api/makeBolRequest";
 dotenv.config();
 
-// console.log(process.env.USER_ODFL);
+// console.log(process.env.ODFL_USER);
 
-console.log("running makeBolRequest()...");
-makeBolRequest();
+// console.log("running makeBolRequest()...");
+// makeBolRequest();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Who is the consignee's cousin?", (answer) => {
+  switch (answer.toLowerCase()) {
+    case "you":
+      console.log("you are the consignees cousin?!?");
+      break;
+    case "h":
+      console.log("Enter 'you'");
+      break;
+    case "help":
+      console.log("Enter 'you'");
+      break;
+    default:
+      console.log("Invalid answer! type 'help' for help");
+  }
+  rl.close();
+});
